044b4cca6250bb8e3f1a69bea66bf0a08af9b3d6
christopherchinia <chinia214@gmail.com>
2022-06-17 23:21:59 -0400

    commit tree ee54034a11f98e6a50135219ad8b30ab877f16af

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // iterate through each number in the array
+  for (let i = 0; i < array.length; i++) {
+    // for the current num, identify a targetSum that adds to the target(targetSum= target- array[i])
+    const targetSum =target - array[i];
+    // iterate throughthe rest of the array
+    for (let num = i+1; num < array.length; num++){
+      // check if any number is our targetSum
+      // if SourceBuffer,return true
+      if(array[num] === targetSum) return true;
+      }
+  }
+    // if i reach the end of the array,return false
+      return false;
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: 0(n^2)
+  space : 0(n)
 */
 
 /* 
   Add your pseudocode here
+  // Write your algorithm here
+  // iterate through each number in the array
+   // for the current num, identify a targetSum that adds to the target(targetSum= target- array[i])
+    // iterate throughthe rest of the array
+     // check if any number is our targetSum
+      // if SourceBuffer,return true
+      // if i reach the end of the array,return false
 */
 
 /*
   Add written explanation of your solution here
+  function hasTargetSum(array, target) {
+  // Write your algorithm here
+  // iterate through each number in the array
+    // for the current num, identify a targetSum that adds to the target(targetSum= target- array[i])
+    // iterate throughthe rest of the array
+      // check if any number is our targetSum
+      // if SourceBuffer,return true
+    // if i reach the end of the array,return false
+      return false;
+
+}
 */
 
 // You can run `node index.js` to view these console logs
